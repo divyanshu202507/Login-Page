d8bd0b9eaf042ec7dfaf68a62d1b238a6ff85085
Divyanshu <divyanshu202507@gmail.com>
2025-10-02 23:18:43 +0530

    02-10

--- src/Left.jsx
+++ src/Left.jsx
@@ -4,15 +4,11 @@ import "./Left.css";
 export default function Left() {
   return (
     <div className="left-side">
-      <h2>Welcome Back👋</h2>
+      <h2>Welcome Back</h2>
       <h3>To</h3>
       <h1>Planora.ai</h1>
 
-      <div className="info">
-        <p>Manage students, track attendance, and access notices → all in one place.
-          Planora.ai makes academics simple, smart, and organized.</p>
-      </div>
-
+      
     </div>
   );
 }
